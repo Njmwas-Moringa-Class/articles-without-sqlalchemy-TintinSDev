c06c2a93787a527150195835145e26284577b34b
Martin Maina <mmaina290@gmail.com>
2024-02-16 22:58:21 +0300

    Update Magazine.py

--- lib/Magazine.py
+++ lib/Magazine.py
@@ -1,22 +1,23 @@
-from . import Article
+from Article import Article
+
 class Magazine:
     
-    _all_magazines = []
+    magazines = []
     
     def __init__(self, name, category):
         self._name = name
         self._category = category
         self._articles = []
-        Magazine._all_magazines.append(self)
+        Magazine.magazines.append(self)
 
     def name(self):
         return self._name
 
     def category(self):
         return self._category
-    
-    def contributors(self):
-        return list(set(article.author() for article in self._articles))
+
+    def articles(self):
+        return self._articles
 
     def add_article(self, author, title):
         new_article = Article(author, self, title)
@@ -25,17 +26,17 @@ class Magazine:
 
     @classmethod
     def all(cls):
-        return cls._all_magazines
+        return cls.magazines
 
     @classmethod
     def find_by_name(cls, name):
         return next((magazine for magazine in cls.magazines if magazine.name() == name), None)
 
-    @classmethod
-    def article_titles(cls):
-        return [article.title() for article in Article.articles if article.magazine() == cls]
+    def article_titles(self):
+        return [article.title() for article in self._articles]
 
-    @classmethod
-    def contributing_authors(cls):
-        return [author for magazine in cls._all_magazines for author in magazine.contributors() if len(author.articles()) > 2]
+    def contributing_authors(self):
+        authors = [article.author() for article in self._articles]
+        author_count = {author: authors.count(author) for author in authors}
+        return [author for author, count in author_count.items() if count > 2]
    
