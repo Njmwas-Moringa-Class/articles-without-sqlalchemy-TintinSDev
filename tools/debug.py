594c5e6252cab36e020c1171534a7f3275c81172
Martin Maina <mmaina290@gmail.com>
2024-02-19 23:44:03 +0300

    Update debug.py

--- tools/debug.py
+++ tools/debug.py
@@ -4,41 +4,7 @@ sys.path.append('lib')  # Assuming 'lib' is one level above 'tools'
 
 import ipdb
 
-from . import Author
-from . import Magazine
-from . import Article
 
-if __name__ == '__main__':
-    #  WRITE YOUR TEST CODE HERE ###
-    
-
-    author1 = Author("Peter")
-    author2 = Author("Martin")
-    
-    ipdb.set_trace()
-    
-    magazine1 = Magazine("Spice Font", "Space World")
-    magazine2 = Magazine("Spice FM", "Citizen TV")
-
-    # Insert a breakpoint here to interact with the terminal
-    ipdb.set_trace()
-
-    article1 = author1.add_article(magazine1, "The Future of AI")
-    article2 = author2.add_article(magazine1, "Coding for Beginners")
-    article3 = author1.add_article(magazine2, "Advancements in Space Exploration")
-
-    # Test code
-    print("Authors:")
-    for author in Author.all():
-        print(author.name())
-
-    print("\nMagazines:")
-    for magazine in Magazine.all():
-        print(f"{magazine.name()} - {magazine.category()}")
-
-    print("\nArticles:")
-    for article in Article.all():
-        print(f"{article.title()} by {article.author().name()} in {article.magazine().name()}")
 
     # DO NOT REMOVE THIS
     ipdb.set_trace()
