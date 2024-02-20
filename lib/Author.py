8a8face7f5e5f6b391b3a8da76858c3ae44d3f18
Martin Maina <mmaina290@gmail.com>
2024-02-20 07:01:20 +0300

    Update Author.py

--- lib/Author.py
+++ lib/Author.py
@@ -5,27 +5,27 @@ class Author:
     
     def __init__(self, name):
         self._name = name
-        Author.authors.append(self)
-        self._articles = []
+        self.__class__.authors.append(self)
+        self._authored_articles = []
         
+      @property
     def name(self):
         return self._name
 
     def articles(self):
-        return self._articles
-    
+        return self._authored_articles
+
+
     def magazines(self):
-        return list(set(article.magazine() for article in self._articles))
+        return list(set(article.magazine for article in self._authored_articles))
 
     def add_article(self, magazine, title):
         new_article = Article(self, magazine, title)
-        self._articles.append(new_article)
-        return new_article
-    
+        self._authored_articles.append(new_article)
+
     def topic_areas(self):
-        return list(set(article.magazine().category() for article in self._articles))
+        return list(set(article.magazine.category for article in self._authored_articles))
 
     @classmethod
     def all(cls):
-        return cls.authors  
-
+        return cls.authors
