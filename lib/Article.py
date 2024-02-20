149924a56a418a81f913a53041574befdeff674f
Martin Maina <mmaina290@gmail.com>
2024-02-20 06:57:47 +0300

    Update Article.py

--- lib/Article.py
+++ lib/Article.py
@@ -1,21 +1,29 @@
 class Article:
+
     articles = []
 
     def __init__(self, author, magazine, title):
+
         self._author = author
         self._magazine = magazine
         self._title = title
-        Article.articles.append(self)
+        self.__class__.articles.append(self)
+        magazine.add_published_article(self)
+        author.add_authored_article(self)
 
+    @property
     def title(self):
         return self._title
 
+    @property
     def author(self):
         return self._author
 
+    @property
     def magazine(self):
         return self._magazine
-    
+
     @classmethod
     def all(cls):
         return cls.articles
+
